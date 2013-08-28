a7afc5960354e6387d663cfc49d1d8a395093145
danielld75 <danielld@poczta.fm>
2013-08-28 07:52:09 +0200

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Parpp::Application.config.secret_key_base = '55abfbc70e79cc61d87f5d273f158bc5f958281768fb6cb4af0d45a11864e0752ff85ff3460b13e0b9bc9f66830807a2e21a603473b4d16e363df61477fc194e'
